bb0a293d72f1f86cbc36266cb3c8d700d0b830c7
cynthialeung <cynthia.nirmata>
2020-04-20 10:09:48 -0700

    Update NirmataPE installation

--- src/test/java/NirmataPE/NirmataPE.java
+++ src/test/java/NirmataPE/NirmataPE.java
@@ -28,7 +28,7 @@ public class NirmataPE  extends NirmataSetup {
     public void installNirmataPE()  {
         String ec2InstanceIP = (new NirmataApplicationProperties()).properties.getProperty("ec2InstanceIP");
         System.out.println(" ================ Install Nirmata PE ======================");
-        System.out.println("++ Update April 19 ++" );
+        System.out.println("++ Last Update: April 20 ++" );
         System.out.println("ec2InstanceIP= "+ec2InstanceIP);
 
         System.out.println("nadmVersion: " + "tar -xf " + nadmVersion +".tar.gz" );
